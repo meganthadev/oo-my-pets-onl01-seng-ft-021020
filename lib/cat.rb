81176a33791e1622d9a7cdc1e9ea127b4bc78730
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 05:02:14 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,20 @@
 class Cat
 
-  attr_accessor :name, :Owner
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+@@all = []
 
   def initialize(name, owner)
     @name = name
+    @mood = "nervous"
     @owner = owner
+    @@all << self
+    owner.cats << self
   end  
+  
+  def self.all
+    @@all
+  end
 
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,20 @@
 class Dog
 
-  attr_accessor :name, :Owner
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+@@all = []
 
   def initialize(name, owner)
     @name = name
+    @mood = "nervous"
     @owner = owner
+    @@all << self
+    owner.dogs << self
   end  
+  
+  def self.all
+    @@all
+  end
 
 end
