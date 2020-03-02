dacffc13e1f8fe07eb9ecca4252a433f94b57155
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 04:54:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -37,6 +37,12 @@ class Owner
     Dog.new(name, self)
   end
   
-  
+  def walk_dogs
+   self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
   
 end  
