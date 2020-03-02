f048bdb527e876218864b6f32e7190c23f94bb47
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 04:43:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,16 @@
 class Owner
 
- attr_accessor :name
+   attr_accessor :cats, :dogs
+  attr_reader :species, :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @species = "human"
+    @@all << self
+    @cats = []
+    @dogs = []
   end
 
 end
