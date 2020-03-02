3da1f603c211772ef6a56fbde37b6871d8ec90be
Megan Horsfield <meganthadev@gmail.com>
2020-03-02 05:02:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,8 @@ class Owner
 
 
   def list_pets
-    
+    number_of_dogs = self.dogs.count
+    number_of_cats = self.cats.count
     "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
   end 
   
